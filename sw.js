ca4a5ebd142242d978e9d96004c7c650764f43e2
medqueue-app <vanilacake86@gmail.com>
2026-07-10 00:20:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,51 +1,52 @@
-const CACHE_NAME = 'medqueue-v4';
+const CACHE_NAME = 'medqueue-v6';
 const ASSETS = [
-  './index.html',
-  './pilih_role.html',
-  './login_pasien.html',
-  './dashboard_pasien.html',
-  './ambil_antrean_pilih_poli.html',
-  './detail_poli.html',
-  './konfirmasi_antrean.html',
-  './barcode_status_antrean.html',
-  './pasien_sedang_dilayani.html',
-  './pelayanan_selesai_pasien.html',
-  './riwayat_antrean.html',
-  './pengaturan.html',
-  './penutup.html',
-  './scan_qr_kode_pasien.html',
-  './hasil_scan_barcode.html',
-  './manifest.json',
-  './medqueue_logo.png',
-  './js/queue.js'
+    './index.html',
+    './dashboard_pasien.html',
+    './dashboard_admin.html',
+    './dashboard_admin_utama.html',
+    './ambil_antrean_pilih_poli.html',
+    './detail_poli.html',
+    './konfirmasi_antrean.html',
+    './barcode_status_antrean.html',
+    './pasien_sedang_dilayani.html',
+    './pelayanan_selesai_pasien.html',
+    './login_admin.html',
+    './login_pasien.html',
+    './daftar_admin.html',
+    './daftar_pasien.html',
+    './scan_barcode_pasien.html',
+    './verifikasi_berhasil.html',
+    './admin_memanggil_antrean_b_11.html',
+    './admin_sedang_melayani.html',
+    './admin_pelayanan_selesai.html',
+    './kelola_daftar_antrean.html',
+    './daftar_antrean_pasien_admin.html',
+    './penutup.html',
+    './js/queue.js'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) return caches.delete(key);
+                })
+            );
         })
-      );
-    })
-  );
+    );
+    self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
 });
